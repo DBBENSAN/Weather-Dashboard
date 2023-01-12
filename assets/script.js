a1b3c9a9f605e693a7e673838ac9c2a3633ce636
DBENSAN <dftb97@gmail.com>
2023-01-11 23:33:33 -0800

    Adds basic read me

--- assets/script.js
+++ assets/script.js
@@ -1,74 +1,87 @@
 var city;
 var cityList;
+var apiKey = `e14ab01b03d592b73f63fd4f13b9a099`
+
+var asideEl = $(".aside").addClass("col-3");
+var innerDiv = $("<div>").addClass("input-group mb-3")
+var inputEl = $("<input>").addClass("form-control city-input").attr("type", "text").attr("placeholder", "Search").attr("id", "city-input");
+var submitButton = $("<button>").addClass("btn btn-outline-secondary").attr("type", "button").append("Submit");
+var listContainer = $("<div>").addClass("row-2").append("Previous Searches:");
+asideEl.append(innerDiv);
+asideEl.append(listContainer);
+innerDiv.append(inputEl);
+innerDiv.append(submitButton);
+
+
 
 $(function () {
-    getRecent();
-    getSearchedList()
-    popAsideEl();
-
-})
-
-function getRecent() {
-    var lastSearch = localStorage.getItem("lastSearch");
-    if (lastSearch === 0) {
-        city = "Berkeley";
-    } else {
-        city = lastSearch
-    } console.log(city)
-}
-
-
-function getSearchedList() {
-    var list = JSON.parse(localStorage.getItem("cityList"));
-    if (list === 0) {
+    cityList = JSON.parse(localStorage.getItem("cityList"));
+    if (cityList === null) {
         cityList = [];
     } else {
-        cityList = list
+        for (var i = 0; i < cityList.length; i++) {
+            var listButton = $("<button>").addClass("row").attr("value", cityList[i].city);
+            listContainer.append(listButton);
+        }
     }
-}
+})
 
-function saveLocalStr() {
-    localStorage.setItem("lastSearch", city);
-    cityList.push(city);
-    localStorage.setItem("cityList", JSON.stringify(cityList))
-}
-
-function popAsideEl() {
-    var asideEl = $(".aside").addClass("col-3");
-    var innerDiv = $("<div>").addClass("input-group mb-3")
-    var inputEl = $("<input>").addClass("form-control city-input").attr("type", "text").attr("placeholder", "Search").attr("id", "city-input");
-    var submitButton = $("<button>").addClass("btn btn-outline-secondary").attr("type", "button").append("Submit");
-
-    var listContainer = $("<div>").addClass("row-2").append("Previous Searches:");
-    
-    asideEl.append(innerDiv);
-    asideEl.append(listContainer);
-    innerDiv.append(inputEl);
-    innerDiv.append(submitButton);
-    // for (i = 0; i < ; i++) {
-    //     console.log("function is running");
-    // }
-
-    submitButton.on("click", () => {
-        $("city-input").val("");
-        getCity();
-
-        search()
-    })
-}
-
-function popMainEl() {
-    var mainEl = $(".main").addClass("col-9");
-}
-
-function getCity() {
-    city = $("#city-input").val();
-    if (!!city || !!cityList.includes(city)) {
-        saveLocalStr();
-        return city;
-    } else {
-        console.log('Not a city')
+submitButton.on("click", function(event) {
+    event.preventDefault();
+    var targetCity = inputEl.val();
+        console.log(targetCity);
+    if (!cityList.includes(targetCity)) {
+        previousButton = $("<button>").text(targetCity).attr("value", targetCity)
+        
     }
+})
+
+
+function getMain(targetCity) {
+    var queryLink = `https://api.openweathermap.org/data/3.0/onecall?${targetCity}&exclude=current&appid=${apikey}`
 }
 
-console.log(localStorage);
+
+
+// function getRecent() {
+//     
+
+
+
+// function saveLocalStr() {
+//     localStorage.setItem("lastSearch", city);
+//     cityList.push(city);
+//     localStorage.setItem("cityList", JSON.stringify(cityList))
+// }
+
+// function popAsideEl() {
+
+//     //populate list
+//     // cityList.forEach((city) => {
+//     //     listContainer.append(city);
+//     // });
+
+
+
+
+//     submitButton.on("click", () => {
+//         $("city-input").val("");
+//         getCity();
+
+//         // search()
+//     })
+// }
+
+// function popMainEl() {
+//     var mainEl = $(".main").addClass("col-9");
+// }
+
+// function getCity() {
+//     city = $("#city-input").val();
+//     if (!!city || !!cityList.includes(city)) {
+//         saveLocalStr();
+//         return city;
+//     } else {
+//         console.log('Not a city')
+//     }
+// }
