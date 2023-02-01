5e30087eaec0e8a5cfc89705027dd0a77257cac6
DBENSAN <dftb97@gmail.com>
2023-02-01 00:31:08 -0800

    refactor project w/ basic styling

--- assets/script.js
+++ assets/script.js
@@ -1,96 +1,104 @@
-var city;
-var cityList;
-var apiKey = `e14ab01b03d592b73f63fd4f13b9a099`
+const API_KEY = "0fb854934bc58130931f8acc673ec22f";
 
-var asideEl = $(".aside").addClass("col-3");
-var innerDiv = $("<div>").addClass("input-group mb-3")
-var inputEl = $("<input>").addClass("form-control city-input").attr("type", "text").attr("placeholder", "Search").attr("id", "city-input");
-var submitButton = $("<button>").addClass("btn btn-outline-secondary").attr("type", "button").append("Submit");
-var listContainer = $("<div>").addClass("row-2").append("Previous Searches:");
-asideEl.append(innerDiv);
-asideEl.append(listContainer);
-innerDiv.append(inputEl);
-innerDiv.append(submitButton);
+const searchInput = document.querySelector("#searchInput");
+const searchButton = document.querySelector("#searchButton");
+const cityMain = document.querySelector("#city");
+const temperatureMain = document.querySelector("#temperature");
+const humidityMain = document.querySelector("#humidity");
+const dateMain = document.querySelector("#mainDate");
+const windMain = document.querySelector("#wind");
+const forecastContainer = document.querySelector("#forecastContainer");
 
+window.addEventListener("load", function () {
+    updatePreviousSearches();
+    updateWeather("Berkeley")
+    updateForecast("Berkeley")
+});
 
-
-$(function () {
-    cityList = JSON.parse(localStorage.getItem("cityList"));
-    if (cityList === null) {
-        cityList = [];
-    } else {
-        for (var i = 0; i < cityList.length; i++) {
-            var listButton = $("<button>").addClass("row").attr("value", cityList[i].city);
-            listContainer.append(listButton);
-        }
+function updatePreviousSearches() {
+    let previousSearches = JSON.parse(localStorage.getItem("previousSearches")) || [];
+    let previousSearchesContainer = document.getElementById("previousSearchesContainer");
+    previousSearchesContainer.innerHTML = "";
+    for (let city of previousSearches) {
+        let previousSearchElement = document.createElement("div");
+        previousSearchElement.innerHTML = city;
+        previousSearchElement.addEventListener("click", function () {
+            updateWeather(city);
+            updateForecast(city);
+        });
+        previousSearchesContainer.appendChild(previousSearchElement);
     }
-})
-
-
-submitButton.on("click", function(event) {
-    event.preventDefault();
-    var targetCity = inputEl.val();
-        console.log(targetCity);
-    if (!cityList.includes(targetCity)) {
-        previousButton = $("<button>").text(targetCity).attr("value", targetCity)
-
-    }
-})
-
-
-function getMain(targetCity) {
-    var queryLink = `https://api.openweathermap.org/data/3.0/onecall?${targetCity}&exclude=current&appid=${apikey}`
 }
 
 
+function updateWeather(city) {
+    return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${API_KEY}`)
+        .then((response) => response.json())
+        .then((data) => {
+            const weatherData = data;
+            const temperature = weatherData.main.temp;
+            const date = new Date(weatherData.dt * 1000).toLocaleDateString();
+            const humidity = weatherData.main.humidity;
+            const cityName = weatherData.name;
+            const windSpeed = weatherData.wind.speed;
+            cityMain.innerText = cityName;
+            dateMain.innerText = date;
+            temperatureMain.innerText = temperature + "°F";
+            humidityMain.innerText = "Humidity: " + humidity + "%";
+            windMain.innerText = "Wind: " + windSpeed + "mph";
+        })
+};
 
-// function getRecent() {
-//     
+function updateForecast(city) {
+    return fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${API_KEY}`)
+        .then((response) => response.json())
+        .then((data) => {
+            // Update the forecast container with the 5 day forecast data
+            const forecastData = data;
+            forecastContainer.innerHTML = "";
+            for (let i = 0; i < 5; i++) {
+                const forecast = forecastData.list[i * 8];
+                const temperature = forecast.main.temp;
+                const weather = forecast.weather[0].description;
+                const date = new Date(forecast.dt * 1000).toLocaleDateString();
+                const windSpeed = forecast.wind.speed;
+                const humidity = forecast.main.humidity;
+                const icon = `http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`;
 
+                const forecastElement = document.createElement("div");
+                forecastElement.classList.add("forecast-card");
+                forecastElement.classList.add("col");
+                forecastElement.innerHTML = `
+                <h2>${date}</h2>
+                <img src="${icon}" alt="${weather}" />
+                <p>Temperature: ${temperature}°F</p>
+                <p>Wind Speed: ${windSpeed} mph</p>
+                <p>Humidity: ${humidity}%</p>
+              `;
+                forecastContainer.appendChild(forecastElement);
+            }
+        });
+}
 
+searchButton.addEventListener("click", function () {
+    const city = searchInput.value;
+    updateWeather(city);
+    updateForecast(city);
+});
 
-// function saveLocalStr() {
-//     localStorage.setItem("lastSearch", city);
-//     cityList.push(city);
-//     localStorage.setItem("cityList", JSON.stringify(cityList))
-// }
+function addToLocalStorage(city) {
+    let previousSearches = JSON.parse(localStorage.getItem("previousSearches")) || [];
+    if (!previousSearches.includes(city)) {
+        previousSearches.push(city);
+        localStorage.setItem("previousSearches", JSON.stringify(previousSearches));
+    }
+}
 
-// function popAsideEl() {
+searchButton.addEventListener("click", function () {
+    const city = searchInput.value;
+    updateWeather(city);
+    updateForecast(city);
+    addToLocalStorage(city);
+    updatePreviousSearches();
+});
 
-//     //populate list
-//     // cityList.forEach((city) => {
-//     //     listContainer.append(city);
-//     // });
-
-
-
-
-//     submitButton.on("click", () => {
-//         $("city-input").val("");
-//         getCity();
-
-//         // search()
-//     })
-// }
-
-// function popMainEl() {
-//     var mainEl = $(".main").addClass("col-9");
-// }
-
-// function getCity() {
-//     city = $("#city-input").val();
-//     if (!!city || !!cityList.includes(city)) {
-//         saveLocalStr();
-//         return city;
-//     } else {
-//         console.log('Not a city')
-//     }
-// }
-
-
-function rentalCarCost(d) {
-    var dayRate = 40
-  
-    if ()
-    // Your solution here
-  }
